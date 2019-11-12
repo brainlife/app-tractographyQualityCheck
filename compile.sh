ae0f81dc53e915c3ffe12e9713fe19169024ee88
Soichi Hayashi <hayashis@iu.edu>
2019-11-12 21:32:19 +0000

    updated to use the latest version of wmatools, soichih@vistasoft, and 2019 matlab

--- compile.sh
+++ compile.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 module unload matlab
-module load matlab/2017a
+module load matlab/2019a
 
 log=compiled/commit_ids.txt
 true > $log
-echo "/N/u/brlife/git/vistasoft" >> $log
-(cd /N/u/brlife/git/vistasoft && git log -1) >> $log
+echo "/N/u/hayashis/git/vistasoft" >> $log
+(cd /N/u/hayashis/git/vistasoft && git log -1) >> $log
 echo "/N/u/brlife/git/jsonlab" >> $log
 (cd /N/u/brlife/git/jsonlab && git log -1) >> $log
 echo "/N/u/brlife/git/wma_tools" >> $log
 (cd /N/u/brlife/git/wma_tools && git log -1) >> $log
 echo "/N/u/brlife/git/encode" >> $log
 (cd /N/u/brlife/git/encode && git log -1) >> $log
-#echo "/N/u/brlife/git/encode" >> $log
-#(cd /N/u/brlife/git/encode && git log -1) >> $log
-#echo "/N/u/brlife/git/spm12" >> $log
-#(cd /N/u/brlife/git/spm12 && git log -1) >> $log
 
 cat > build.m <<END
-addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/hayashis/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/wma_tools'))
 addpath(genpath('/N/u/brlife/git/encode'))
-addpath(genpath('/N/soft/rhel7/mrtrix/3.0/mrtrix3/matlab'))
-mcc -m -R -nodisplay -d compiled  bsc_feAndSegQualityCheck_BL_v2
+%addpath(genpath('/N/soft/rhel7/mrtrix/3.0/mrtrix3/matlab'))
+mcc -m -R -nodisplay -d compiled main
 
 exit
 END
